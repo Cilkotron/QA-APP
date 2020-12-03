8f2fb0d458d4ad54eaa5a55631f82d9b11c78b55
Sanja Budic <sanjabudic2907@gmail.com>
2020-12-03 13:03:33 +0100

    Display questions

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,6 @@ class User extends Authenticatable
     use Notifiable;
     use TwoFactorAuthenticatable;
 
-
     /**
      * The attributes that are mass assignable.
      *
@@ -59,10 +58,4 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
-
-    public function questions() {
-        return $this->hasMany(Question::class);
-    }
-
-
 }
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia\Inertia::render('Dashboard');
-})->name('dashboard');
+Route::resource('/question', 'QuestionController');
+
+
+
+
+
+
+
