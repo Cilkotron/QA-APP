2227b7f8c040361862c97022848bac725af97b4a
Sanja Budic <sanjabudic2907@gmail.com>
2020-12-28 15:25:42 +0100

    Update best answer on question table

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -36,10 +36,17 @@ class Answer extends Model
             $answer->question->increment('answers_count');
             $answer->question->save();
         });
+        static::deleted(function ($answer) {
+            $answer->question->decrement('answers_count');
+        });
     }
 
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans();
+    }
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 }
