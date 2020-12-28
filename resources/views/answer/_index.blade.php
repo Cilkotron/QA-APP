9098aea5d89f2c2e90064936c154f6046a5b1a9a
Sanja Budic <sanjabudic2907@gmail.com>
2020-12-28 13:56:28 +0100

    Update best_answer_id on questions table

--- resources/views/answer/_index.blade.php
+++ resources/views/answer/_index.blade.php
@@ -18,7 +18,7 @@
                             <a title="This answer is not useful" class="vote-down off">
                                 <i class="fas fa-caret-down fa-3x"></i>
                             </a>
-                            <a title="Mark this answer as best answer (Click again to undo)" class="vote-accepted mt-2">
+                            <a title="Mark this answer as best answer (Click again to undo)" class="{{ $answer->status }} mt-2">
                                 <i class="fas fa-check fa-2x"></i>
                                 <span class="favorites-count">12</span>
                             </a>
